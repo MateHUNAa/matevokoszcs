e4321859d795f4790bf77888af4ad3a333cff411
MateHUN <matehunter123gmail.com>
2021-03-04 13:04:04 +0100

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -11,9 +11,9 @@ var usedMemory = os.totalmem() -os.freemem(), totalMemory = os.totalmem();
 var  getpercentage = 
   ((usedMemory/totalMemory) * 100).toFixed(2) + '%'
 
-message.channel.send("Memory used in GB" + (usedMemory/ Math.pow(1024, 3)).toFixed(2))
-message.channel.send("Used memory" + getpercentage);
+message.channel.send("Memory used in GB " + (usedMemory/ Math.pow(1024, 3)).toFixed(2))
+message.channel.send("Used memory " + getpercentage);
 var os = require('os');
-message.channel.send(`${os.cpus()}`);
+console.log(chalk.blue(`${os.cpus()}`));
     }
 };
