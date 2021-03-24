a14b0a015aff3aa33d9a72aaac7c81d2cd2da366
MateHUNAa <65528914+MateHUNAa@users.noreply.github.com>
2021-03-24 12:59:27 +0100

    Create supportserver.js

---
+++ commands/supportserver.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "support",
+    description: "Link support server.",
+    async execute(message, args) {
+        
+        return message.member.send("https://discord.gg/eFgwF8dNx3");
+
+    }
+};
+  
